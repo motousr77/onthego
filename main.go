2f5e6479973cf5b3c3764c044765d598db74e164
Anatoly <motousr77@gmail.com>
2022-07-29 19:30:40 +0000

    gin-gonic/gin

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"net/http"
 	"onthego/common"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	var node = common.SomeNode{Node: nil, Value: "Adam"}
 	node.Print()
+
+	var rt = gin.Default()
+	rt.GET("/check", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "OK")
+	})
+	rt.Run("localhost:8000")
 }
 
 
 /*
-some test )
+curl https://checkip.amazonaws.com/
 */
